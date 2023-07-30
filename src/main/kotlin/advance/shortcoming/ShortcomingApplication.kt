2949f1eaa4554af654e3bec03fc5aef3dcb1c943
Hanjae Jung <hanjae.jung@eigene.ai>
2023-07-30 18:58:31 +0900

    init

---
+++ src/main/kotlin/advance/shortcoming/ShortcomingApplication.kt
@@ -0,0 +1,11 @@
+package advance.shortcoming
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ShortcomingApplication
+
+fun main(args: Array<String>) {
+	runApplication<ShortcomingApplication>(*args)
+}
---
+++ src/test/kotlin/advance/shortcoming/ShortcomingApplicationTests.kt
@@ -0,0 +1,13 @@
+package advance.shortcoming
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShortcomingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
